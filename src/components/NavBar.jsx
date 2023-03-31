535babf1486bb8c3aceecfdb75158a5ff08294fe
Hamza <hamzaghnaimm99@gmail.com>
2023-04-01 00:20:30 +0300

    basically finished navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {
   Toolbar,
   Typography,
   Box,
-  Link,
+  ButtonGroup,
   Button,
   Stack,
 } from "@mui/material";
@@ -14,16 +14,38 @@ const NavBar = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h5">Shortly</Typography>
-          <Link href="#">Link</Link>
-          <Stack direction={"row"} spacing={2} sx={{ flexGrow: 1 }}>
-            <Button color="inherit">Features</Button>
-            <Button color="inherit">Pricing</Button>
-            <Button color="inherit">Resources</Button>
+          <Typography variant="h5" sx={{ mr: 4 }}>
+            Shortly
+          </Typography>
+          <Stack
+            display={"block"}
+            direction={"row"}
+            spacing={3}
+            sx={{ flexGrow: 1 }}
+          >
+            <ButtonGroup variant="text">
+              <Button style={{ fontSize: "9px" }} color="inherit">
+                Features
+              </Button>
+              <Button style={{ fontSize: "9px" }} color="inherit">
+                Pricing
+              </Button>
+              <Button style={{ fontSize: "9px" }} color="inherit">
+                Resources
+              </Button>
+            </ButtonGroup>
           </Stack>
-          <Stack direction={"row"} spacing={2}>
-            <Button color="inherit">Login</Button>
-            <Button variant="contained" color="inherit">
+          <Stack direction={"row"} spacing={4}>
+            <Button style={{ fontSize: "9px" }} size="small" color="inherit">
+              Login
+            </Button>
+            <Button
+              style={{ fontSize: "9px" }}
+              size="small"
+              variant="contained"
+              color="inherit"
+              sx={{ borderRadius: 28 }}
+            >
               Sign Up
             </Button>
           </Stack>
