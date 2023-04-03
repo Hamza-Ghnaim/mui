9a1390050383867b5c9bcee24af30e1896e17fd0
Hamza <hamzaghnaimm99@gmail.com>
2023-04-03 17:00:20 +0300

    NavBar responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 import { ReactComponent as Logo } from "../images/Shortly.svg";
-import { AppBar, Toolbar, Box, Button, Stack, IconButton } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Button,
+  Stack,
+  IconButton,
+  useMediaQuery,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
 const NavBar = ({ theme }) => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setOpenDrawer(open);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" elevation={0}>
@@ -10,28 +37,34 @@ const NavBar = ({ theme }) => {
           <IconButton variant="h5" sx={{ mr: 4 }}>
             <Logo />
           </IconButton>
-          <Stack
-            display={"block"}
-            direction={"row"}
-            spacing={2}
-            sx={{ flexGrow: 1 }}
-          >
-            <Button
-              sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
+          {isMobile ? (
+            <IconButton onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <Stack
+              display={"block"}
+              direction={"row"}
+              spacing={2}
+              sx={{ flexGrow: 1 }}
             >
-              Features
-            </Button>
-            <Button
-              sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
-            >
-              Pricing
-            </Button>
-            <Button
-              sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
-            >
-              Resources
-            </Button>
-          </Stack>
+              <Button
+                sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
+              >
+                Features
+              </Button>
+              <Button
+                sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
+              >
+                Pricing
+              </Button>
+              <Button
+                sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
+              >
+                Resources
+              </Button>
+            </Stack>
+          )}
           <Stack direction={"row"} spacing={2}>
             <Button
               sx={{ fontSize: "10px", color: theme.palette.neutral.main }}
@@ -48,10 +81,6 @@ const NavBar = ({ theme }) => {
                 borderRadius: 28,
                 bgcolor: theme.palette.primary.main,
                 color: "#ffffff",
-                "&:hover": {
-                  backgroundColor: "#bbdefb",
-                  color: "#ffffff",
-                },
               }}
             >
               Sign Up
@@ -59,6 +88,35 @@ const NavBar = ({ theme }) => {
           </Stack>
         </Toolbar>
       </AppBar>
+      <Drawer anchor="right" open={openDrawer} onClose={toggleDrawer(false)}>
+        <List>
+          <ListItem Button>
+            <ListItemText primary="Features" />
+          </ListItem>
+          <ListItem Button>
+            <ListItemText primary="Pricing" />
+          </ListItem>
+          <ListItem Button>
+            <ListItemText primary="Resources" />
+          </ListItem>
+          <ListItem Button>
+            <ListItemText primary="Login" />
+          </ListItem>
+          <ListItem Button>
+            <Button
+              variant="contained"
+              color="inherit"
+              sx={{
+                borderRadius: 28,
+                bgcolor: theme.palette.primary.main,
+                color: "#ffffff",
+              }}
+            >
+              Sign Up
+            </Button>
+          </ListItem>
+        </List>
+      </Drawer>
     </Box>
   );
 };
