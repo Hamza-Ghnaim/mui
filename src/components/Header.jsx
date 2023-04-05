2ac046e51ec6ef6b95df3bc3c2821f751188bfdb
Hamza <hamzaghnaimm99@gmail.com>
2023-04-06 01:23:58 +0300

    Header responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,80 +7,122 @@ import {
   CardContent,
   CardActions,
   Button,
+  useMediaQuery,
 } from "@mui/material";
 import React from "react";
 
 const Header = ({ theme }) => {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Card
-            sx={{
-              minWidth: 275,
-              boxShadow: "none",
-              borderRadius: "none",
-              mt: 3,
-              ml: 2,
-            }}
-          >
-            <CardContent>
-              <Typography variant="h2" fontWeight="bold" sx={{ mt: 9, mb: 1 }}>
-                More than just
-                <br />
-                shorter links
-              </Typography>
+        {isSmallScreen ? (
+          <>
+            <Grid item xs={12}>
+              <Paper elevation={0}>
+                <img
+                  src={require("../images/working.svg").default}
+                  alt="My Image"
+                  style={{ maxWidth: "100%" }}
+                />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sx={{ textAlign: "center" }}>
+              <Card
+                sx={{
+                  minWidth: 275,
+                  boxShadow: "none",
+                  borderRadius: "none",
+                  backgroundColor: "transparent",
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    variant="h2"
+                    fontWeight="bold"
+                    sx={{ mt: 4, mb: 1 }}
+                  >
+                    More than just
+                    <br />
+                    shorter links
+                  </Typography>
 
-              <Typography
-                variant="body2"
-                sx={{
-                  mb: 2,
-                  color: theme,
-                  color: theme.palette.neutral.main,
-                }}
+                  <Typography variant="body2" sx={{ mb: 2, color: theme }}>
+                    Build your brand's recognition and get detailed
+                    <br />
+                    insights on how your links are performing
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{ justifyContent: "center" }}>
+                  <Button
+                    variant="contained"
+                    color="inherit"
+                    sx={{
+                      pr: 4,
+                      pl: 4,
+                      borderRadius: 28,
+                      bgcolor: theme.palette.primary.main,
+                      color: "#ffffff",
+                    }}
+                  >
+                    Get Started
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid item xs={6}>
+              <Card
+                sx={{ minWidth: 275, boxShadow: "none", borderRadius: "none" }}
               >
-                Build your brand's recognition and get detailed
-                <br />
-                insights on how your links are performing
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                variant="contained"
-                color="inherit"
-                sx={{
-                  pr: 4,
-                  pl: 4,
-                  borderRadius: 28,
-                  bgcolor: theme.palette.primary.main,
-                  color: "#ffffff",
-                  boxShadow: 0,
-                  "&:hover": {
-                    bgcolor: "#b2ebf2",
-                    boxShadow: 0,
-                  },
-                }}
-              >
-                Get Started
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper elevation={0}>
-            <img
-              src={require("../images/working.svg").default}
-              alt="My Image"
-              style={{
-                width: "48%",
-                maxWidth: "100%",
-                position: "absolute",
-                right: -110,
-                bottom: 90,
-              }}
-            />
-          </Paper>
-        </Grid>
+                <CardContent>
+                  <Typography
+                    variant="h2"
+                    fontWeight="bold"
+                    sx={{ mt: 4, mb: 1 }}
+                  >
+                    More than just
+                    <br />
+                    shorter links
+                  </Typography>
+
+                  <Typography variant="body2" sx={{ mb: 2, color: theme }}>
+                    Build your brand's recognition and get detailed
+                    <br />
+                    insights on how your links are performing
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    color="inherit"
+                    sx={{
+                      pr: 4,
+                      pl: 4,
+                      borderRadius: 28,
+                      bgcolor: theme.palette.primary.main,
+                      color: "#ffffff",
+                    }}
+                  >
+                    Get Started
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper elevation={0}>
+                <img
+                  src={require("../images/working.svg").default}
+                  alt="My Image"
+                  style={{ maxWidth: "100%" }}
+                />
+              </Paper>
+            </Grid>
+          </>
+        )}
       </Grid>
     </Box>
   );
