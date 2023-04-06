1fe0d5c8f85e4d24e3387d6b6ba93e63072156b1
Hamza <hamzaghnaimm99@gmail.com>
2023-04-06 15:16:03 +0300

    Header Responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,87 +12,12 @@ import {
 import React from "react";
 
 const Header = ({ theme }) => {
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <Box>
       <Grid container spacing={2}>
-        {isLargeScreen ? (
-          <>
-            <Grid item xs={6}>
-              <Card
-                sx={{
-                  minWidth: 275,
-                  boxShadow: "none",
-                  borderRadius: "none",
-                  mt: 3,
-                  ml: 2,
-                }}
-              >
-                <CardContent>
-                  <Typography
-                    variant="h2"
-                    fontWeight="bold"
-                    sx={{ mt: 9, mb: 1 }}
-                  >
-                    More than just
-                    <br />
-                    shorter links
-                  </Typography>
-
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      mb: 2,
-                      color: theme,
-                      color: theme.palette.neutral.main,
-                    }}
-                  >
-                    Build your brand's recognition and get detailed
-                    <br />
-                    insights on how your links are performing
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    variant="contained"
-                    color="inherit"
-                    size="large"
-                    sx={{
-                      pr: 4,
-                      pl: 4,
-                      borderRadius: 28,
-                      bgcolor: theme.palette.primary.main,
-                      color: "#ffffff",
-                      boxShadow: 0,
-                      "&:hover": {
-                        bgcolor: "#b2ebf2",
-                        boxShadow: 0,
-                      },
-                    }}
-                  >
-                    Get Started
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper elevation={0}>
-                <img
-                  src={require("../images/working.svg").default}
-                  alt="My Image"
-                  style={{
-                    width: "47%",
-                    maxWidth: "100%",
-                    position: "absolute",
-                    right: -120,
-                    bottom: 240,
-                  }}
-                />
-              </Paper>
-            </Grid>
-          </>
-        ) : (
+        {isSmallScreen ? (
           <>
             <Grid item xs={12}>
               <Paper elevation={0}>
@@ -145,6 +70,80 @@ const Header = ({ theme }) => {
                   </Button>
                 </CardActions>
               </Card>
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid item xs={6}>
+              <Card
+                sx={{
+                  minWidth: 275,
+                  boxShadow: "none",
+                  borderRadius: "none",
+                  mt: 3,
+                  ml: 2,
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    variant="h2"
+                    fontWeight="bold"
+                    sx={{ mt: 9, mb: 1 }}
+                  >
+                    More than just
+                    <br />
+                    shorter links
+                  </Typography>
+
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      mb: 2,
+                      color: theme,
+                      color: theme.palette.neutral.main,
+                    }}
+                  >
+                    Build your brand's recognition and get detailed
+                    <br />
+                    insights on how your links are performing
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    color="inherit"
+                    sx={{
+                      pr: 4,
+                      pl: 4,
+                      borderRadius: 28,
+                      bgcolor: theme.palette.primary.main,
+                      color: "#ffffff",
+                      boxShadow: 0,
+                      "&:hover": {
+                        bgcolor: "#b2ebf2",
+                        boxShadow: 0,
+                      },
+                    }}
+                  >
+                    Get Started
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper elevation={0}>
+                <img
+                  src={require("../images/working.svg").default}
+                  alt="My Image"
+                  style={{
+                    width: "48%",
+                    maxWidth: "100%",
+                    position: "absolute",
+                    right: -110,
+                    bottom: 90,
+                  }}
+                />
+              </Paper>
             </Grid>
           </>
         )}
