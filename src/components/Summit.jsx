406c203526c518cc13d84843817e4d27030cd33e
Hamza <hamzaghnaimm99@gmail.com>
2023-04-07 17:37:13 +0300

    Summit Responsiveness

--- src/components/Summit.jsx
+++ src/components/Summit.jsx
@@ -23,8 +23,129 @@ const Summit = ({ theme }) => {
         alignItems: "center",
       }}
     >
+      <Divider
+        orientation="horizontal"
+        flexItem={true}
+        sx={{
+          borderColor: theme.palette.primary.main,
+          borderWidth: "4px",
+          width: 15,
+          position: "absolute",
+          left: 835.5,
+          bottom: -515,
+        }}
+      />
+      <Divider
+        orientation="horizontal"
+        flexItem={true}
+        sx={{
+          borderColor: theme.palette.primary.main,
+          borderWidth: "4px",
+          width: 15,
+          position: "absolute",
+          left: 511,
+          bottom: -515,
+        }}
+      />
+      <Box
+        sx={{
+          backgroundColor: theme.palette.primary.dark,
+          borderRadius: "50%",
+          width: "70px",
+          height: "70px",
+          position: "absolute",
+          left: 270,
+          bottom: -405,
+          transform: "translateX(-50%)",
+          // bottom: "-100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 1,
+        }}
+      >
+        <img
+          src={require("../images/recognition.svg").default}
+          alt="Circle Image"
+          style={{
+            width: "50%",
+            height: "50%",
+            clipPath: "circle(100%)", // make the image circular
+            zIndex: 1,
+          }}
+        />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.primary.dark,
+          borderRadius: "50%",
+          width: "70px",
+          height: "70px",
+          position: "absolute",
+          left: 915,
+          bottom: -480,
+          transform: "translateX(-50%)",
+          // bottom: "-100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 1,
+        }}
+      >
+        <img
+          src={require("../images/pencils.svg").default}
+          alt="Circle Image"
+          style={{
+            width: "50%",
+            height: "50%",
+            clipPath: "circle(100%)", // make the image circular
+            zIndex: 1,
+          }}
+        />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.primary.dark,
+          borderRadius: "50%",
+          width: "70px",
+          height: "70px",
+          position: "absolute",
+          left: 596,
+          bottom: -445,
+          transform: "translateX(-50%)",
+          // bottom: "-100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 1,
+        }}
+      >
+        <img
+          src={require("../images/records.svg").default}
+          alt="Circle Image"
+          style={{
+            width: "50%",
+            height: "50%",
+            clipPath: "circle(100%)", // make the image circular
+            zIndex: 1,
+          }}
+        />
+      </Box>
+      {/* <img
+        src={require("../images/pencils.svg").default}
+        alt="Circle Image"
+        style={{
+          position: "absolute",
+          left: 910,
+          bottom: -471,
+          transform: "translateX(-50%)",
+          // bgcolor: theme.palette.primary.dark,
+          clipPath: "circle(100%)", // make the image circular
+          zIndex: 1,
+        }}
+      /> */}
       <br />
-      <Box sx={{ mt: 30, mb: 20 }}>
+      <Box sx={{ mt: 30 }}>
         <Typography variant="h5" fontWeight="bold" sx={{ pl: 7.9, mb: 2 }}>
           Advanced Statistics
         </Typography>
@@ -50,12 +171,13 @@ const Summit = ({ theme }) => {
           our advanced statistics dashboard
         </Typography>
       </Box>
-      <Box sx={{ mb: 80 }}>
+      <Box sx={{ mb: 55 }}>
         <Grid container spacing={3}>
           <Grid item xs={4}>
             <Card
               sx={{
                 width: "300px",
+                pt: 5,
                 position: "absolute",
                 left: 210,
                 bottom: -580,
@@ -79,16 +201,12 @@ const Summit = ({ theme }) => {
               </CardContent>
             </Card>
           </Grid>
-          <Divider
-            orientation="vertical"
-            flexItem={true}
-            sx={{ borderColor: "blue", borderWidth: "4px" }}
-          />
           <Grid item xs={4}>
             <Card
               sx={{
                 width: "300px",
                 position: "absolute",
+                pt: 5,
                 left: 535,
                 bottom: -620,
               }}
@@ -111,16 +229,12 @@ const Summit = ({ theme }) => {
               </CardContent>
             </Card>
           </Grid>
-          <Divider
-            orientation="horizontal"
-            flexItem={true}
-            sx={{ borderColor: "blue", borderWidth: "20px" }}
-          />
           <Grid item xs={4}>
             <Card
               sx={{
                 width: "300px",
                 position: "absolute",
+                pt: 5,
                 left: 860,
                 bottom: -660,
               }}
@@ -132,7 +246,6 @@ const Summit = ({ theme }) => {
                 <Typography
                   variant="body2"
                   sx={{
-                    color: theme,
                     color: theme.palette.neutral.main,
                   }}
                 >
