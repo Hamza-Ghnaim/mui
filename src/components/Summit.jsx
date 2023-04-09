cae41a678094dbf7a90030827962ecb61b588a9a
Hamza <hamzaghnaimm99@gmail.com>
2023-04-09 13:17:22 +0300

    hi there

--- src/components/Summit.jsx
+++ src/components/Summit.jsx
@@ -5,6 +5,7 @@ import {
   CardContent,
   useMediaQuery,
   Divider,
+  Stack,
   Grid,
 } from "@mui/material";
 import React from "react";
@@ -27,7 +28,123 @@ const Summit = ({ theme }) => {
       {isSmallScreen ? (
         <>
           <Box sx={{ mt: 30 }}>
-            <Typography variant="h5" fontWeight="bold" sx={{ mb: 2 }}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.dark,
+                borderRadius: "50%",
+                width: "70px",
+                height: "70px",
+                position: "absolute",
+                left: "157px",
+                transform: "translateY(150px)",
+                // bottom: "-100px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                zIndex: 1,
+              }}
+            >
+              <img
+                src={require("../images/recognition.svg").default}
+                alt="Circle Image"
+                style={{
+                  width: "50%",
+                  height: "50%",
+                  clipPath: "circle(100%)", // make the image circular
+                  zIndex: 1,
+                }}
+              />
+            </Box>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.dark,
+                borderRadius: "50%",
+                width: "70px",
+                height: "70px",
+                position: "absolute",
+                left: "157px",
+                transform: "translateY(783px)",
+                // bottom: "-100px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                zIndex: 1,
+              }}
+            >
+              <img
+                src={require("../images/pencils.svg").default}
+                alt="Circle Image"
+                style={{
+                  width: "50%",
+                  height: "50%",
+                  clipPath: "circle(100%)", // make the image circular
+                  zIndex: 1,
+                }}
+              />
+            </Box>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.dark,
+                borderRadius: "50%",
+                width: "70px",
+                height: "70px",
+                position: "absolute",
+                left: "157px",
+                transform: "translateY(455px)",
+                // bottom: "-100px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                zIndex: 1,
+              }}
+            >
+              <img
+                src={require("../images/records.svg").default}
+                alt="Circle Image"
+                style={{
+                  width: "50%",
+                  height: "50%",
+                  clipPath: "circle(100%)", // make the image circular
+
+                  zIndex: 1,
+                }}
+              />
+            </Box>
+            <Divider
+              orientation="vertical"
+              flexItem={true}
+              sx={{
+                borderColor: theme.palette.primary.main,
+                borderWidth: "4px",
+                // width: 5,
+                height: 50,
+                position: "absolute",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                transform: "translateY(397px)",
+                // bottom: -515,
+              }}
+            />
+            <Divider
+              orientation="vertical"
+              flexItem={true}
+              sx={{
+                borderColor: theme.palette.primary.main,
+                borderWidth: "4px",
+                // width: 5,
+                height: 50,
+                position: "absolute",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                transform: "translateY(725px)",
+                // bottom: -515,
+              }}
+            />
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              sx={{ mb: 2, textAlign: "center" }}
+            >
               Advanced Statistics
             </Typography>
             <Typography
@@ -37,6 +154,7 @@ const Summit = ({ theme }) => {
                 color: theme.palette.neutral.main,
                 fontWeight: 500,
                 textAlign: "center",
+                mb: 10,
               }}
             >
               Track how your links are performing
@@ -45,93 +163,69 @@ const Summit = ({ theme }) => {
               <br />
               statistics dashboard
             </Typography>
-          </Box>
-          <Box sx={{ mb: 90 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={4}>
-                <Card
-                  sx={{
-                    width: "300px",
-                    pt: 5,
-                    position: "absolute",
-                    left: 210,
-                    bottom: -580,
-                  }}
-                >
-                  <CardContent>
-                    <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                      Brand Recognition
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{
-                        color: theme,
-                        color: theme.palette.neutral.main,
-                      }}
-                    >
-                      Build your brand's recognition and get detailed
-                      <br />
-                      insights on how your links are performing
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={4}>
-                <Card
-                  sx={{
-                    width: "300px",
-                    position: "absolute",
-                    pt: 5,
-                    left: 535,
-                    bottom: -620,
-                  }}
-                >
-                  <CardContent>
-                    <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                      Detailed Records
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{
-                        color: theme,
-                        color: theme.palette.neutral.main,
-                      }}
-                    >
-                      Build your brand's recognition and get detailed
-                      <br />
-                      insights on how your links are performing
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={4}>
-                <Card
-                  sx={{
-                    width: "300px",
-                    position: "absolute",
-                    pt: 5,
-                    left: 860,
-                    bottom: -660,
-                  }}
-                >
-                  <CardContent>
-                    <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                      Fully Customizable
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{
-                        color: theme.palette.neutral.main,
-                      }}
-                    >
-                      Build your brand's recognition and get detailed
-                      <br />
-                      insights on how your links are performing
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
+            <Stack
+              spacing={12.5}
+              direction="column"
+              alignItems="center"
+              sx={{ mb: 90, textAlign: "center" }}
+            >
+              <Card sx={{ width: "300px", pt: 5 }}>
+                <CardContent>
+                  <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
+                    Brand Recognition
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: theme,
+                      color: theme.palette.neutral.main,
+                    }}
+                  >
+                    Boost your brand recognition with each click. Genericlinks
+                    don't <br />
+                    mean a thing.Branded links help <br />
+                    instil confidencein your content.
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ width: "300px", pt: 5 }}>
+                <CardContent>
+                  <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
+                    Detailed Records
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: theme,
+                      color: theme.palette.neutral.main,
+                    }}
+                  >
+                    Gain insights into who's clicing <br />
+                    your links. Knowing when <br />
+                    and where people engage with your <br />
+                    content helpsinform better <br /> decisions
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ width: "300px", pt: 5 }}>
+                <CardContent>
+                  <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
+                    Fully Customizable
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      color: theme.palette.neutral.main,
+                    }}
+                  >
+                    Improved brand awarness and <br />
+                    content diecoverability through <br />
+                    customizable links. supersearching <br />
+                    audiance engagement.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Stack>
           </Box>
         </>
       ) : (
@@ -293,9 +387,9 @@ const Summit = ({ theme }) => {
                         color: theme.palette.neutral.main,
                       }}
                     >
-                      Build your brand's recognition and get detailed
-                      <br />
-                      insights on how your links are performing
+                      Boost your brand recognition with each click. Genericlinks
+                      don't mean a thing.Branded links help instil confidencein
+                      your content.
                     </Typography>
                   </CardContent>
                 </Card>
@@ -321,9 +415,9 @@ const Summit = ({ theme }) => {
                         color: theme.palette.neutral.main,
                       }}
                     >
-                      Build your brand's recognition and get detailed
-                      <br />
-                      insights on how your links are performing
+                      Gain insights into who's clicing your links. Knowing when
+                      and where people engage with your content helpsinform
+                      better decisions
                     </Typography>
                   </CardContent>
                 </Card>
@@ -348,9 +442,9 @@ const Summit = ({ theme }) => {
                         color: theme.palette.neutral.main,
                       }}
                     >
-                      Build your brand's recognition and get detailed
-                      <br />
-                      insights on how your links are performing
+                      Improved brand awarness and content diecoverability
+                      through customizable links. supersearching audiance
+                      engagement.
                     </Typography>
                   </CardContent>
                 </Card>
